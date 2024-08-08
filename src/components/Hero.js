f6367c4d697840418f142737fef18991c5b2274b
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-08-08 15:32:57 +0100

    Refactored Hero component: removed unnecessary line, reformatted code, and added type attribute to h1 element.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-
 const Hero = () => {
   return (
     <motion.div
@@ -10,13 +9,16 @@ const Hero = () => {
       transition={{ duration: 0.5 }}
       className="hero-container"
     >
-        {/* Add your hero content here */} 
-        <div className="container mx-auto px-4 text-center">
-
-      <h1>Welcome to our Hairstyling Boutique</h1>
-      <p>Discover the latest trends and styles in hairstyling.</p>
-      <button type="button" className="text-white   bg-blue-800  hover:decoration-blue-600 focus:ring-inherit  px-2.5 me2.5 mb-2 dark:bg-blue-300 dark:hover:border-e-orange-950 focus:outline-none dark:ring-blue-200"             >Book an Appointment</button>
-    </div>
+      <div className="container mx-auto px-4 text-center">
+        <h1 type=" h1" className=" ">Welcome to our Hairstyling Boutique</h1>
+        <p>Discover the latest trends and styles in hairstyling.</p>
+        <button
+          type="button"
+          className="text-white bg-blue-800 hover:decoration-blue-600 focus:ring-inherit px-2.5 me2.5 mb-2 dark:bg-blue-300 dark:hover:border-e-orange-950 focus:outline-none dark:ring-blue-200"
+        >
+          Book an Appointment
+        </button>
+      </div>
     </motion.div>
   );
 };
